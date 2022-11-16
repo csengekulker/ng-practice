600fdce1e52beda0bb974cb05a461e302b438a71
csengekulker <k71608883429@ktch.hu>
2022-11-16 11:45:42 +0100

    generated class usage

--- src/app/emp/emp.component.ts
+++ src/app/emp/emp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-emp',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emp.component.scss']
 })
 export class EmpComponent implements OnInit {
-
+  employees !: Employee[]
   constructor() { }
 
   ngOnInit(): void {
+    this.employees = [
+      new Employee(1, "John Doe", "NY"),
+      new Employee(2, "Jane Doe", "NY"),
+      new Employee(3, "John Doe", "NY")
+    ]
   }
 
 }
